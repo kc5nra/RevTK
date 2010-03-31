bca801fc940507282feb1d157f7ad15a0ec3f7f7
Fabrice Denis <fabrice.denis@gmail.com>
2010-04-01 00:47:43 +0200

    Kanji logo and screens images update on the home page.
    * Nicer kanji logo for the homepage
    * Removed the side bar links to give a little more space to sponsors
    * Updated the screenies to reflect the 'refactored' flashcard stacks
      and review page... FINALLY (x_X)'

--- apps/revtk/modules/home/templates/_homeSideView.php
+++ apps/revtk/modules/home/templates/_homeSideView.php
@@ -1,14 +1,14 @@
 <div class="col-side">
-  <div style="margin-top:20px">
-    <?php echo image_tag('/images/1.0/nav/home_kanji.gif', 'size="140x142"') ?>
-  </div>
+  <?php echo image_tag('/images/1.0/nav/home_kanji_2.gif', 'size="131x131" style="margin:0 0 0 5px;"') ?>
+<?php /*  
   <div id="homelinks">
     <?php echo link_to('Support the website', 'about/support', array('id'=>'donatelink')) ?>
     <?php echo link_to('News Archive', 'news/index') ?>
     <?php echo link_to('Contact', 'home/contact') ?>
     <?php echo link_to('About', 'about/index') ?>
-  </div>
-
+  </div>*/
+?>
+  <p style="margin:10px 0 10px;font:bold 18px/1.2em Georgia, serif;text-align:center;color:#858376;">Sponsors</p>
 <?php use_helper('LocalAssets'); echo get_local_content(__FILE__, 'partners'); ?>
 
 </div>
