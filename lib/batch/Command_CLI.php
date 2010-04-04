6b14927a41a0f078364aae6a4d1c114adcca7f17
Fabrice Denis <fabrice.denis@gmail.com>
2010-04-04 16:49:46 +0200

    Command_CLI normalized way of printing out the command line usage, added sample script file.

--- lib/batch/Command_CLI.php
+++ lib/batch/Command_CLI.php
@@ -6,6 +6,17 @@
  *   --v         Verbose mode
  *   --help      Show help
  * 
+ * Basic methods:
+ *   getOption($name[, $default])     Return option, or default value, or throws an error
+ *   throwError($message[, args])     Writes messages to STDERR and quits
+ *   verbose($message[, args])        Sprintf style message display only in verbose mode (--v)
+ *
+ * Useful methods:
+ *   getRelativePathFrom($path, $base)
+ * 
+ * TODO
+ *   --app <app>       Sets application (changes the settings file, defaults to "revtk")
+ *   --env <env>       Sets environment (changes the environment, defaults to "dev")
  * 
  * @author  Fabrice Denis
  */
@@ -26,7 +37,37 @@ class Command_CLI
   protected 
     $isVerbose = true,
     $args      = null;
+
+  const
+    /**
+     * Used with rtrim() or ltrim() to clean the ends of path names.
+     */
+    SLASHES_WHITESPACE  = " \t\n\r\\/";
   
+  /**
+   * Description of the command line tool, including list of flags.
+   *
+   * Below is an example, you should override it in the extending class.
+   *
+   * This array of data will be formatted in a consistent way for all command
+   * line tools. The verbose flag (--v) will always be added at the end of the list
+   * of flags.
+   *
+   */
+  protected
+    $cmdHelp = array(
+      'short_desc' => 'Example file to create command line tools.',
+      'usage_fmt'  => '[--v] --foo <file> [--bar]',
+      'options'    => array(
+        'foo <file>'     => "Description of the option flag. Descriptions spanning multiple\nlines can use the newline character.",
+        'bar'            => 'Enable bar mode (defaults to "baz").'
+      )
+    );
+  
+  /**
+   * Remember to call parent::init() first when you override this!
+   *
+   */
   public function init()
   {
     $this->args = new SimpleArgs();
@@ -47,17 +88,36 @@ class Command_CLI
    */
   protected function showHelp()
   {
-    echo <<<EOL
-Command that does something and other kinds of things.
+    global $argv;
+    
+    define('COL_ALIGN', 24);
+    define('OPT_PREFIX', '  --');
 
-php dosomething.php [--v] --config <path> --out <file> 
+    if (!isset($this->cmdHelp)) {
+      $this->throwError('Help text must be declared in extending class, see '.__CLASS__.' documentation.');
+    }
 
-  --config <file>     Lorem ipsum dolor sit amet, lorem ipsumus
-  --out <file>        A javascript or ipsum dolor sit amet, lorem ipsumus
-  
-  --v                 Verbose mode (off by default)
+    $help = $this->cmdHelp;
+    
+    // short description
+    echo $help['short_desc']."\n\n";
 
-EOL;
+    // sample command line
+    echo 'php ' . $argv[0] . ' ' . $help['usage_fmt']."\n\n";
+
+    // print out list of flags
+    foreach ($help['options'] as $optFlag => $optDesc)
+    {
+      $align  = max(COL_ALIGN - strlen(OPT_PREFIX) - strlen($optFlag), 1);
+
+      // align newlines to the description column
+      $optDesc = preg_replace('/\n/', "\n".str_repeat(' ', COL_ALIGN), $optDesc);
+
+      echo OPT_PREFIX.$optFlag.str_repeat(' ', $align).$optDesc."\n";
+    }
+
+    // add the verbose flag to the list
+    echo "\n".OPT_PREFIX.'v'.str_repeat(' ', COL_ALIGN - strlen(OPT_PREFIX) - 1).'Verbose mode (off by default)'."\n";
   }
 
   /**
@@ -78,6 +138,14 @@ EOL;
     return $value !== false ? $value : $default;
   }
 
+  /**
+   * Prints out sprintf style error message to STDERR and exits.
+   * 
+   * @param string $message
+   * @param mixed  $arguments   Variable number of sprintf style arguments
+   *
+   * @return void
+   */
   protected function throwError()
   {
     $args = func_get_args();
@@ -86,6 +154,14 @@ EOL;
     exit(-1);
   }
   
+  /**
+   * If verbose flag is set, prints a sprintf style message, otherwise do nothing.
+   *
+   * @param string $message
+   * @param mixed  $arguments   Variable number of sprintf style arguments
+   * 
+   * @return void
+   */
   protected function verbose()
   {
     $args = func_get_args();
@@ -93,6 +169,27 @@ EOL;
       $message = call_user_func_array('sprintf', $args) . "\n";
       fwrite(STDERR, $message);
     }
+  }
+
+  /**
+   * Return a relative path from an absolute path, given the base path.
+   * 
+   * @param  string $path  Fully qualified source path, can include filename
+   * @param  string $base  Fully qualified base path (no filename)
+   *
+   * @return string        Relative path, without leading separator
+   */
+  protected function getRelativePathFrom($path, $base)
+  {
+    $pos = strpos($path, $base);
+    if ($pos === false || $pos !== 0) {
+
+      $this->throwError('getRelativePathFrom() path (%s) does not start with base (%s)', $path, $base);
+    }
+    
+    $relPath = substr($path, strlen($base));
+    
+    return ltrim($relPath, self::SLASHES_WHITESPACE);
   }
 }
 
